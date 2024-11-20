ff3b1534a505fcf01cd884b14cee5e08351c3a39
Антон <katrinnnn.tgr@gmail.com>
2024-11-20 17:50:05 +0300

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,5 @@
+first_name ='Курс: Python'
+Number_of_tasks = 12
+Number_of_hours_spent = 1.5
+Time_per_task = (Number_of_tasks // Number_of_hours_spent)
+print(first_name, ':всего задач',  Number_of_tasks, 'затраченно часов:', Number_of_hours_spent, 'среднее время выполнения:', Time_per_task / 64, 'часа')
